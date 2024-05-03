109536a261e4a99758d8433ef9614792c55119e2
Johan Viljoen <johan.viljoen@bbd.co.za>
2024-05-03 09:06:59 +0200

    Route implementations

--- Server/routes/modifiers.js
+++ Server/routes/modifiers.js
@@ -4,10 +4,85 @@ const express = require('express');
 const { pool } = require('../db');
 const router = express.Router();
 
-// Get all
-router.get('/', (req, res) => {
+// Get total mult
+router.get('/multiplier', (req, res) => {
+  const difficulty = req.query.difficulty;
+  const boardSize = req.query.boardSize;
 
-    res.json("modifiers endpoint");
+  if (difficulty === undefined || boardSize === undefined) {
+    return res.status(500).json({ error: 'Please provide difficulty and boardSize'});
+  }
+
+  const query = `
+        SELECT (d.ScoreMult * b.ScoreMult) AS [Multiplier]
+        FROM dbo.Difficulty d, dbo.BoardSize b
+        WHERE d.Difficultyname = '${difficulty}' AND b.SizeDescription = '${boardSize}'
+  `;
+
+  pool.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: 'Failed SQL'});
+
+    }
+    else if (result.recordset.length === 0) {
+      return res.status(404).json({ error: 'Could not find combination'});
+    }
+    else {
+        
+      const finalMult = result.recordset[0].Multiplier;
+      res.json({ Multiplier: finalMult});
+    }
+
+  });
+});
+
+
+router.get('/boardsize', (req, res) => {
+
+  const query = `
+  SELECT d.SizeDescription
+  FROM dbo.BoardSize d
+  `;
+
+  pool.query(query, (err, result) => {
+  if (err) {
+    return res.status(500).json({ error: 'Failed SQL'});
+
+  }
+  else if (result.recordset.length === 0) {
+    return res.status(404).json({ error: 'Could not find any board sizes'});
+  }
+  else {      
+    res.json(result.recordset);
+  }
+
+  });
+
+});
+
+
+router.get('/difficulty', (req, res) => {
+
+  const query = `
+  SELECT d.Difficultyname
+  FROM dbo.Difficulty d
+  `;
+
+  pool.query(query, (err, result) => {
+  if (err) {
+    return res.status(500).json({ error: 'Failed SQL'});
+
+  }
+  else if (result.recordset.length === 0) {
+    return res.status(404).json({ error: 'Could not find any difficulties'});
+  }
+  else {      
+    res.json(result.recordset);
+  }
+
+  });
+
 });
 
 
--- Server/routes/users.js
+++ Server/routes/users.js
@@ -2,10 +2,18 @@
 
 var express = require('express');
 var router = express.Router();
+var { pool } = require('../db');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var playerName = req.user.userName;
+
+  if (playerName !== '') {
+      res.json({ userName: playerName});
+  } 
+  else {
+      res.status(404).json({ error : 'Cannot get highscore for anonymous player!' });
+  }
 });
 
 module.exports = router;
