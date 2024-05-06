044ceba9f1a24d69641ccf4b7394e176982ae454
Johan Viljoen <johan.viljoen@bbd.co.za>
2024-05-06 18:48:37 +0200

    Removed vulnerability in Registration step

--- Server/app.js
+++ Server/app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
