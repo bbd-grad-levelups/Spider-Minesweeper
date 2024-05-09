6191fcb5d3c2cf33885ab5254e5e8fa2a8ba6b21
Johan Viljoen <johan.viljoen@bbd.co.za>
2024-05-09 12:19:44 +0200

    Remove https?

--- Front end/components/logIn/logInFunctionality.js
+++ Front end/components/logIn/logInFunctionality.js
@@ -1,6 +1,6 @@
 const clientId = 'Iv23liHbiQ9aD6uAgyym';
-const redirectUri = 'https://bbd-grad-levelups.github.io/Spider-Minesweeper/';
+const redirectUri = 'http://bbd-grad-levelups.github.io/Spider-Minesweeper/';
 
 function authorize() {
-  window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user`;
+  window.location.href = `http://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user`;
 }
